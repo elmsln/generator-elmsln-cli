388c104307633404777e9985eac4402d4fb59fdb
Michael Potter <mgp140@gmail.com>
2018-06-13 16:51:12 -0400

    book2jos now creates files for each body tag.

--- generators/books:book2jos/index.js
+++ generators/books:book2jos/index.js
@@ -1,8 +1,11 @@
+// @ts-check
+
 'use strict';
 const ElmsGenerator = require('../ElmsGenerator');
 const drupal2jos = require('drupal-book-2-jos');
-const fs = require('fs');
+const fs = require('fs-extra');
 const Path = require('path');
+const Confirm = require('prompt-confirm');
 
 module.exports = class extends ElmsGenerator {
   constructor(args, opts) {
@@ -58,9 +61,40 @@ module.exports = class extends ElmsGenerator {
     const path = this.env.path;
     const output = this.env.output;
     const xml = fs.readFileSync(Path.join(path));
-    const jos = await drupal2jos(xml);
+    // Get the jos from the xml file
+    let jos = await drupal2jos(xml);
+    // Format the jos to include a location if it doesn't already have one.
+    // we'll use the item id since we know it's unique
+    jos.items = jos.items.map(item => {
+      return Object.assign({}, item, { location: item.location || `${item.id}.html` });
+    });
     if (jos) {
-      fs.writeFileSync(output, JSON.stringify(jos, null, 2));
+      // Make the output folder
+      const exists = fs.existsSync(output);
+      // If it already exists then give the user the option of backing out
+      if (exists) {
+        const prompt = new Confirm(
+          'That folder already exists. Would you like to replace it?'
+        );
+        const confirmReplace = await prompt.run();
+        if (!confirmReplace) {
+          console.log('exiting process');
+          return;
+        }
+      }
+      // Create the folder
+      fs.ensureDirSync(output);
+      // Loop through each of the items and create files
+      for (let item of jos.items) {
+        fs.writeFileSync(Path.join(output, item.location), item.body);
+      }
+      // Remove the body information from the outline
+      jos.items = jos.items.map(item => {
+        delete item.body;
+        return item;
+      });
+      // Write the mong
+      fs.writeFileSync(Path.join(output, 'outline.json'), JSON.stringify(jos, null, 2));
     }
   }
 
